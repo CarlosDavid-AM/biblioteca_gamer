929f8c004df68e491fd29ca714d21210f5a75a73
carlos <apolayamendozacarlos@gmail.com>
2025-10-17 16:48:39 -0500

    Se crea el metodo para eliminar un registro

--- src/controller/juegosController.js
+++ src/controller/juegosController.js
@@ -55,3 +55,22 @@ exports.obtenerJuegosById = async (req, res) => {
     res.status(500).json({ message: "Error Interno en el Servidor" });
   }
 };
+
+exports.eliminarJuego = async (req, res) => {
+  const { id } = req.params;
+
+  const sql = "DELETE FROM juegos WHERE id = ?";
+
+  try {
+    const [result] = await db.query(sql, [id]);
+
+    if (result.affectedRows === 0) {
+      return res.status(404).json({ message: "No encontramos el juego" });
+    }
+
+    res.status(200).json({ message: "Juego eliminado correctamente" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error Interno en el Servidor" });
+  }
+};
--- src/router/juegosRouter.js
+++ src/router/juegosRouter.js
@@ -5,5 +5,6 @@ const juegosController = require("../controller/juegosController");
 
 router.get("/juegos", juegosController.obtenerJuegos);
 router.get("/juegos/:id", juegosController.obtenerJuegosById);
+router.delete("/juegos/:id", juegosController.eliminarJuego);
 
 module.exports = router;
